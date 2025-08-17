2cb052984738b3504af14cf589e447e146fc75e1
Rajat Mourya <rajatmourya82@gmail.com>
2025-08-17 11:14:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/signup/page.tsx
+++ src/app/(app)/signup/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/od';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { useRouter } from 'next/navigation';
