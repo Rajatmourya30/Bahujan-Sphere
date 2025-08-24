929f50ceff34330a47a5d48c4a6198f8c48a0dc6
Rajat Mourya <rajatmourya82@gmail.com>
2025-08-24 09:20:14 +0000

    undo last 4 changes i want my old icon

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -9,39 +9,26 @@ export function Logo({ className }: LogoProps) {
     <svg
       width="32"
       height="32"
-      viewBox="0 0 40 40"
+      viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={cn(className)}
+      className={cn("text-primary", className)}
     >
-      <g clipPath="url(#clip0_30_2)">
-        <path
-          d="M20 5.00003L35 12.5V27.5L20 35L5 27.5V12.5L20 5.00003Z"
-          stroke="hsl(var(--primary))"
-          strokeWidth="3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M5 12.5L20 20L35 12.5"
-          stroke="hsl(var(--primary))"
-          strokeWidth="3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M20 35V20"
-          stroke="hsl(var(--primary))"
-          strokeWidth="3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_30_2">
-          <rect width="40" height="40" fill="white" />
-        </clipPath>
-      </defs>
+      <path
+        d="M6.66663 16L2.66663 18.3125V13.6875L6.66663 16Z"
+        fill="currentColor"
+      />
+      <path
+        d="M16 4L2.66663 11.5V20.5L16 28L29.3333 20.5V11.5L16 4Z"
+        stroke="currentColor"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M25.3334 16L29.3334 18.3125V13.6875L25.3334 16Z"
+        fill="currentColor"
+      />
     </svg>
   );
 }
