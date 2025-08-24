f6a3f815f687010396e86a1898cda2f9666d47d7
Rajat Mourya <rajatmourya82@gmail.com>
2025-08-24 09:19:09 +0000

    our old icon is much better then this use our old icon

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -9,35 +9,39 @@ export function Logo({ className }: LogoProps) {
     <svg
       width="32"
       height="32"
-      viewBox="0 0 100 100"
+      viewBox="0 0 40 40"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={cn(className)}
     >
-      <defs>
-        <linearGradient id="logoGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" style={{ stopColor: '#FF8A00', stopOpacity: 1 }} />
-          <stop offset="100%" style={{ stopColor: '#E52E71', stopOpacity: 1 }} />
-        </linearGradient>
-      </defs>
-      <rect width="100" height="100" rx="24" ry="24" fill="url(#logoGradient)" />
-      <g transform="translate(25, 20) scale(0.6)">
+      <g clipPath="url(#clip0_30_2)">
         <path
-          d="M56.2,27.9c-8.9-8.9-23.3-8.9-32.2,0c-8.9,8.9-8.9,23.3,0,32.2c8.9,8.9,23.3,8.9,32.2,0C65.1,51.2,65.1,36.8,56.2,27.9z M35,55.5c-3.5,0-6.8-1.4-9.2-3.8c-2-2-3.2-4.6-3.6-7.3c-0.3-2.3-0.1-4.7,0.7-6.9c1-2.9,2.8-5.3,5.2-7.1c2.9-2.2,6.5-3.3,10.2-2.9c3.3,0.3,6.4,1.8,8.8,4.2c2.4,2.4,3.8,5.5,4.2,8.8c0.3,3.6-0.7,7.2-2.9,10.2c-1.8,2.4-4.2,4.2-7.1,5.2C39.7,55.4,37.3,55.6,35,55.5z"
-          fill="none"
-          stroke="#FFFFFF"
-          strokeWidth="6"
+          d="M20 5.00003L35 12.5V27.5L20 35L5 27.5V12.5L20 5.00003Z"
+          stroke="hsl(var(--primary))"
+          strokeWidth="3"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
         <path
-          d="M49.8,28.8c-2-2.7-4.6-4.8-7.6-6.1c-3.4-1.5-7.2-2-10.9-1.5c-3.4,0.5-6.6,2-9.2,4.4c-2.7,2.4-4.7,5.5-5.7,9c-1,3.2-1,6.6,0,9.9c0.9,3,2.6,5.7,4.9,7.9c2.7,2.5,6,4.1,9.6,4.5c3.6,0.4,7.2-0.2,10.4-1.8c3.1-1.5,5.7-3.8,7.6-6.7"
-          fill="none"
-          stroke="#FFFFFF"
-          strokeWidth="6"
+          d="M5 12.5L20 20L35 12.5"
+          stroke="hsl(var(--primary))"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M20 35V20"
+          stroke="hsl(var(--primary))"
+          strokeWidth="3"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
       </g>
+      <defs>
+        <clipPath id="clip0_30_2">
+          <rect width="40" height="40" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   );
 }
