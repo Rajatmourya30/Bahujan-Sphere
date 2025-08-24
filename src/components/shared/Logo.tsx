fd41d4c79b4b202f0991bce2cd6c765ce2ca6d42
Rajat Mourya <rajatmourya82@gmail.com>
2025-08-24 09:31:51 +0000

    this was our icon but you changed it can you make this iocn back

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -15,15 +15,15 @@ export function Logo({ className }: LogoProps) {
       className={cn("text-primary", className)}
     >
       <path
-        d="M6.66663 16L2.66663 18.3125V13.6875L6.66663 16Z"
-        fill="currentColor"
-      />
-      <path
         d="M16 4L2.66663 11.5V20.5L16 28L29.3333 20.5V11.5L16 4Z"
         stroke="currentColor"
         strokeWidth="2.5"
         strokeLinecap="round"
         strokeLinejoin="round"
+      />
+      <path
+        d="M6.66663 16L2.66663 18.3125V13.6875L6.66663 16Z"
+        fill="currentColor"
       />
       <path
         d="M25.3334 16L29.3334 18.3125V13.6875L25.3334 16Z"
